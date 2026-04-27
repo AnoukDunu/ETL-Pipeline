fb8b03e63fbd6c3342933c60c3778f4de577f79f
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-04-28 00:55:49 +1000

    idk

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # This is the main file.
 # This will connect the PostgreSQL database and run the application.
 
-import psycopg2  # type: ignore[import]
+import psycopg2
 import config as config
 
 # creating the function to connect to the PostgreSQL database
