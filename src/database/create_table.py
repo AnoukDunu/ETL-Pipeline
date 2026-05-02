8b1a3fca7eeeb4ecbd60bae550e52269a0e2da2e
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-05-02 15:11:39 +1000

    added more references in comments

--- src/database/create_table.py
+++ src/database/create_table.py
@@ -2,3 +2,6 @@
 
 # Apparently this database/table creation code needs to be included and can run in the load.py file/function itself.
 # Not to future self to maybe implement this if it makes sense
+
+# Maybe refer to this article for more information (https://medium.com/@anandparayan/building-a-production-ready-etl-pipeline-my-complete-data-engineering-journey-with-python-41fb849471a9)
+# and maybe this youtube video (https://www.youtube.com/watch?v=G0crKzK9Ayk)
