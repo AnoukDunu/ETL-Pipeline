15de520c4b9adbd8f41b37c5f01260db0eb6224e
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-05-19 13:11:27 +1000

    added comments and notes for future implementation

--- src/database/create_table.sql
+++ src/database/create_table.sql
@@ -11,3 +11,5 @@ CREATE TABLE customers (
 );
 
 -- entered the table creation SQL in here as reference. This table will be used to store the transformed data after it has been loaded into the database. The table includes columns for customer information such as their name, contact details, location, company, and the date they joined. The created_at column will automatically store the timestamp of when each record is inserted into the table.
+-- the database table will be created using the psql terminal seperately.
+-- later on, I will look into creating the table using SQLAlchemy in the load.py script to automate the process of creating the table if it does not already exist. After getting the basics of course.
