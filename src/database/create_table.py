88c9bbe4516962975de63a840eac49d739160f5e
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-05-20 18:25:30 +1000

    removed irrelevant file. Cleaning up directory

--- src/database/create_table.py
+++
@@ -1,7 +0,0 @@
-# This file will contain the table creation code for the database. Here you can customize the table structure and the data types of the columns as per your requirements. You can also add any constraints or indexes to the table if needed.
-
-# Apparently this database/table creation code needs to be included and can run in the load.py file/function itself.
-# Not to future self to maybe implement this if it makes sense
-
-# Maybe refer to this article for more information (https://medium.com/@anandparayan/building-a-production-ready-etl-pipeline-my-complete-data-engineering-journey-with-python-41fb849471a9)
-# and maybe this youtube video (https://www.youtube.com/watch?v=G0crKzK9Ayk)
