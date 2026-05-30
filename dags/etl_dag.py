0f7dce5fa0886edab5f8cd1283cb8cfbf667165c
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-05-30 13:53:03 +1000

    configured the DAG file further for Airflow

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -6,6 +6,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2023, 1, 1),
+    'email': ['example@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -18,4 +19,14 @@ dag = DAG(
     description='ETL Pipeline for processing customer data',
     schedule_interval=timedelta(days=1),
     catchup=False,
+)
+
+run_etl = BashOperator(
+    task_id='run_etl',
+    # Airflow's BashOperator allows you to execute a bash command or script as part of your DAG. In this case, we are using it to run the main.py script that contains our ETL pipeline. The command 'python ../src/main.py' tells Airflow to execute the main.py script located in the src directory, which is one level up from the dags directory where this DAG file is located.
+    # Normally its best practice to use absolute paths in Airflow because the BashOperator runs the command on the worker machine, not inside the editor. But its not portable so I opted into a relative path in case of the repo being cloned.
+    # if it was to be an absolute path, it would look something like this:
+    # bash_command='python /Users/anouk/Projects/ETL-Pipeline/src/main.py',
+    bash_command='python ../src/main.py',
+    dag=dag,
 )
