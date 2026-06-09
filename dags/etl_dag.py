9edd704da7dacc58638699cb80eb9ae80a96eccc
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-06-09 20:44:42 +1000

    DAG files not detected. Creating trial DAGs to test syntax and structure

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -1,32 +1,24 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime, timedelta
+from datetime import datetime
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
-    'email': ['example@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'start_date': datetime(2025, 8, 24),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG(
-    'etl_dag',
+with DAG(
+    'etl_sales_data',
     default_args=default_args,
-    description='ETL Pipeline for processing customer data',
-    schedule_interval=timedelta(days=1),
-    catchup=False,
-)
+    description='ETL pipeline for sales_data',
+    schedule_interval='@daily',  # runs every day
+    catchup=False
+) as dag:
 
-run_etl = BashOperator(
-    task_id='run_etl',
-    # Airflow's BashOperator allows you to execute a bash command or script as part of your DAG. In this case, we are using it to run the main.py script that contains our ETL pipeline. The command 'python ../src/main.py' tells Airflow to execute the main.py script located in the src directory, which is one level up from the dags directory where this DAG file is located.
-    # Normally its best practice to use absolute paths in Airflow because the BashOperator runs the command on the worker machine, not inside the editor. But its not portable so I opted into a relative path in case of the repo being cloned.
-    # if it was to be an absolute path, it would look something like this:
-    # bash_command='python /Users/anouk/Projects/ETL-Pipeline/src/main.py',
-    bash_command='python ../src/main.py',
-    dag=dag,
-)
+    run_etl = BashOperator(
+        task_id='run_etl_script',
+        bash_command='.venv/bin/python'
+        ' src/main.py'                   #  'C:\\Users\\Gita\\OneDrive\\Documents\\ETL_Project\\scripts\\etl_process.py'
+    )
